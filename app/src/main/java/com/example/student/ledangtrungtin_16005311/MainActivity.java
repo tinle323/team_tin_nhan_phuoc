caafc08f5249f9351dd2fc3adbe676fdfa9caa8d
tinle323 <skytsoccer04@gmail.com>
2019-03-04 09:57:44 +0700

    Hello

--- app/src/main/java/com/example/student/ledangtrungtin_16005311/MainActivity.java
+++ app/src/main/java/com/example/student/ledangtrungtin_16005311/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.student.ledangtrungtin_16005311;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void CustomListView(View view) {
+        Intent it = new Intent(MainActivity.this, CustomListView.class);
+        startActivity(it);
     }
 }
